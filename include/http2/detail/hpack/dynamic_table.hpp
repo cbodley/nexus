afbcc4536dcfe4918e5b1df1a4ff7b4aa760556e
Casey Bodley <cbodley@gmail.com>
2019-03-05 19:59:22 -0500

    hpack: add dynamic_table
    Signed-off-by: Casey Bodley <cbodley@gmail.com>

---
+++ include/http2/detail/hpack/dynamic_table.hpp
@@ -0,0 +1,107 @@
+#pragma once
+
+#include <deque>
+#include <string>
+#include <vector>
+
+namespace http2::detail::hpack {
+
+template <typename SizeType = uint32_t,
+          SizeType entry_size_overhead = 32,
+          typename Allocator = std::allocator<char>>
+class basic_dynamic_table {
+  using size_type = SizeType;
+  using allocator_type = Allocator;
+
+  using storage_type = std::vector<char, allocator_type>;
+  storage_type storage;
+
+  struct segment { size_type offset; size_type length; };
+  struct entry { segment name, value; };
+  using deque_allocator = typename std::allocator_traits<allocator_type>::
+      template rebind_alloc<entry>;
+  std::deque<entry, deque_allocator> entries;
+
+  size_type size;
+  size_type free;
+  size_type pos = 0;
+
+  void read(std::string& dst, const segment& src, char* end) {
+    dst.resize(src.length);
+    char* data = storage.data() + src.offset;
+    const size_t remaining = std::distance(data, end);
+    if (src.length <= remaining) {
+      dst.assign(data, src.length);
+    } else {
+      dst.assign(data, remaining); // wrap at end
+      dst.append(storage.data(), src.length - remaining);
+    }
+  }
+  void write(segment& dst, const std::string_view& src, char* end) {
+    dst.offset = pos;
+    dst.length = src.size();
+    char* data = storage.data() + dst.offset;
+    const size_t remaining = std::distance(data, end);
+    if (dst.length <= remaining) {
+      pos += src.copy(data, dst.length);
+    } else {
+      src.copy(data, remaining); // wrap at end
+      pos = src.copy(storage.data(), dst.length - remaining, remaining);
+    }
+  }
+
+ public:
+  basic_dynamic_table(size_t max_size, allocator_type alloc = allocator_type())
+    : storage(max_size, alloc),
+      entries(alloc),
+      size(max_size),
+      free(max_size)
+  {}
+
+  void set_size(size_t new_size) {
+    while (size > free + new_size) {
+      auto& e = entries.back();
+      free += e.name.length + e.value.length + entry_size_overhead;
+      entries.pop_back();
+    }
+    size = std::min(new_size, storage.size());
+  }
+  bool lookup(uint32_t index, std::string* name, std::string* value) {
+    if (index >= entries.size()) {
+      return false;
+    }
+    auto& e = entries[index];
+    const auto end = storage.data() + storage.size();
+    if (name) {
+      read(*name, e.name, end);
+    }
+    if (value) {
+      read(*value, e.value, end);
+    }
+    return true;
+  }
+  bool insert(std::string_view name, std::string_view value) {
+    size_t esize = name.size() + value.size() + entry_size_overhead;
+    if (esize > size) {
+      entries.clear();
+      return false;
+    }
+    while (free < esize) {
+      auto& e = entries.back();
+      free += e.name.length + e.value.length + entry_size_overhead;
+      entries.pop_back();
+    }
+    free -= esize;
+
+    entries.emplace_front();
+    auto& e = entries.front();
+    const auto end = storage.data() + storage.size();
+    write(e.name, name, end);
+    write(e.value, value, end);
+    return true;
+  }
+};
+
+using dynamic_table = basic_dynamic_table<>;
+
+} // namespace http2::detail::hpack
---
+++ test/test_hpack_dynamic_table.cc
@@ -0,0 +1,152 @@
+#include <http2/detail/hpack/dynamic_table.hpp>
+#include <gtest/gtest.h>
+
+namespace http2::detail::hpack {
+
+// typedef for a table with overhead=0 to make some math easier
+using dynamic_table_0 = basic_dynamic_table<uint32_t, 0>;
+
+TEST(HPACKDynamicTable, lookup_null)
+{
+  dynamic_table table(4096);
+
+  const std::string NAME = "foo";
+  const std::string VALUE = "bar";
+  ASSERT_TRUE(table.insert(NAME, VALUE));
+  {
+    std::string name;
+    ASSERT_TRUE(table.lookup(0, &name, nullptr));
+    EXPECT_EQ(NAME, name);
+  }
+  {
+    std::string value;
+    ASSERT_TRUE(table.lookup(0, nullptr, &value));
+    EXPECT_EQ(VALUE, value);
+  }
+  EXPECT_TRUE(table.lookup(0, nullptr, nullptr));
+}
+
+TEST(HPACKDynamicTable, insert_mutable)
+{
+  dynamic_table table(4096);
+
+  std::string str = "foo";
+  ASSERT_TRUE(table.insert(str, str));
+  {
+    std::string name, value;
+    ASSERT_TRUE(table.lookup(0, &name, &value));
+    EXPECT_EQ("foo", name);
+    EXPECT_EQ("foo", value);
+  }
+  str = "bar"; // mutate source string
+  { // lookup returns initial values
+    std::string name, value;
+    ASSERT_TRUE(table.lookup(0, &name, &value));
+    EXPECT_EQ("foo", name);
+    EXPECT_EQ("foo", value);
+  }
+}
+
+TEST(HPACKDynamicTable, insert_empty_value)
+{
+  // tests that the per-entry overhead is 32 bytes
+  {
+    dynamic_table table31(31);
+    std::string str = "";
+    EXPECT_FALSE(table31.insert(str, str));
+  }
+  {
+    dynamic_table table32(32);
+    std::string str = "";
+    EXPECT_TRUE(table32.insert(str, str));
+
+    std::string name, value;
+    ASSERT_TRUE(table32.lookup(0, &name, &value));
+    EXPECT_TRUE(name.empty());
+    EXPECT_TRUE(value.empty());
+  }
+}
+
+TEST(HPACKDynamicTable, insert_too_big)
+{
+  dynamic_table table(80);
+  {
+    std::string str(32, 'q');
+    ASSERT_FALSE(table.insert(str, str)); // 32 + 32 + 32 = 96
+    EXPECT_FALSE(table.lookup(0, nullptr, nullptr));
+  }
+  {
+    std::string str = "";
+    ASSERT_TRUE(table.insert(str, str)); // 32
+    ASSERT_TRUE(table.insert(str, str)); // 32
+    EXPECT_TRUE(table.lookup(0, nullptr, nullptr));
+    EXPECT_TRUE(table.lookup(1, nullptr, nullptr));
+  }
+  {
+    std::string str(32, 'q');
+    ASSERT_FALSE(table.insert(str, str)); // 32 + 32 + 32 = 96
+    EXPECT_FALSE(table.lookup(0, nullptr, nullptr)); // drops empty entries
+  }
+}
+
+TEST(HPACKDynamicTable, insert_evict)
+{
+  dynamic_table table(80);
+  {
+    std::string str(32, 'q');
+    ASSERT_FALSE(table.insert(str, str)); // 32 + 32 + 32 = 96
+    EXPECT_FALSE(table.lookup(0, nullptr, nullptr));
+  }
+  {
+    std::string str = "";
+    ASSERT_TRUE(table.insert(str, str)); // 32
+    ASSERT_TRUE(table.insert(str, str)); // 32
+    EXPECT_TRUE(table.lookup(0, nullptr, nullptr));
+    EXPECT_TRUE(table.lookup(1, nullptr, nullptr));
+  }
+  {
+    std::string str(32, 'q');
+    ASSERT_FALSE(table.insert(str, str)); // 32 + 32 + 32 = 96
+    EXPECT_FALSE(table.lookup(0, nullptr, nullptr)); // drops empty entries
+  }
+}
+
+TEST(HPACKDynamicTable, insert_wrap_name)
+{
+  dynamic_table_0 table(2);
+  ASSERT_TRUE(table.insert("0", "")); // offset=0 length=1
+  ASSERT_TRUE(table.insert("ab", "")); // offset=1 length=2
+  std::string name;
+  ASSERT_TRUE(table.lookup(0, &name, nullptr));
+  EXPECT_EQ("ab", name);
+  EXPECT_FALSE(table.lookup(1, nullptr, nullptr));
+}
+
+TEST(HPACKDynamicTable, insert_wrap_value)
+{
+  dynamic_table_0 table(2);
+  ASSERT_TRUE(table.insert("", "0")); // offset=0 length=1
+  ASSERT_TRUE(table.insert("", "ab")); // offset=1 length=2
+  std::string value;
+  ASSERT_TRUE(table.lookup(0, nullptr, &value));
+  EXPECT_EQ("ab", value);
+  EXPECT_FALSE(table.lookup(1, nullptr, nullptr));
+}
+
+TEST(HPACKDynamicTable, set_size_evict)
+{
+  dynamic_table_0 table(4);
+  ASSERT_TRUE(table.insert("a", ""));
+  ASSERT_TRUE(table.insert("b", ""));
+  ASSERT_TRUE(table.insert("c", ""));
+  ASSERT_TRUE(table.insert("d", ""));
+  table.set_size(2);
+  std::string name;
+  ASSERT_TRUE(table.lookup(0, &name, nullptr));
+  EXPECT_EQ("d", name);
+  ASSERT_TRUE(table.lookup(1, &name, nullptr));
+  EXPECT_EQ("c", name);
+  EXPECT_FALSE(table.lookup(2, nullptr, nullptr));
+}
+
+} // namespace http2::detail::hpack
