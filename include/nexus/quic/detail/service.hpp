2588f65735f7eaa3e60fda473d9bea9be5c8c6ab
Casey Bodley <cbodley@gmail.com>
2021-10-09 14:36:48 -0400

    service: add mutex and comments
    Signed-off-by: Casey Bodley <cbodley@gmail.com>

--- include/nexus/quic/detail/service.hpp
+++ include/nexus/quic/detail/service.hpp
@@ -1,7 +1,8 @@
 #pragma once
 
+#include <mutex>
 #include <boost/intrusive/list.hpp>
-#include <asio.hpp>
+#include <asio/execution_context.hpp>
 
 namespace nexus::quic::detail {
 
@@ -9,13 +10,23 @@ struct service_tag {};
 using service_list_base_hook = boost::intrusive::list_base_hook<
     boost::intrusive::tag<service_tag>>;
 
-// requirements for T:
-// inherits publicly from service_list_base_hook
-// has member function service_shutdown()
-template <typename T>
+/// service for two-phase execution_context shutdown, which breaks ownership
+/// cycles between completion handlers and their io objects. tracks objects
+/// which may have outstanding completion handlers, and calls their member
+/// function service_shutdown() when the execution_context is shutting down.
+/// this member function should destroy any memory associated with its
+/// outstanding completion handlers
+///
+/// requirements for IoObject:
+/// * inherits publicly from service_list_base_hook
+/// * has public member function service_shutdown()
+template <typename IoObject>
 class service : public asio::execution_context::service {
   using base_hook = boost::intrusive::base_hook<service_list_base_hook>;
-  boost::intrusive::list<T, base_hook> entries;
+  boost::intrusive::list<IoObject, base_hook> entries;
+  std::mutex mutex;
+
+  /// called by the execution_context on shutdown
   void shutdown() override {
     while (!entries.empty()) {
       auto& entry = entries.front();
@@ -30,10 +41,14 @@ class service : public asio::execution_context::service {
   explicit service(asio::execution_context& ctx)
       : asio::execution_context::service(ctx) {}
 
-  void add(T& entry) {
+  /// register an io object for notification of service_shutdown()
+  void add(IoObject& entry) {
+    auto lock = std::scoped_lock{mutex};
     entries.push_back(entry);
   }
-  void remove(T& entry) {
+  /// unregister an object
+  void remove(IoObject& entry) {
+    auto lock = std::scoped_lock{mutex};
     if (entries.empty()) {
       // already shut down
     } else {
