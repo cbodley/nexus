3c921b047ba4d55abf0aad5a68b03009939dfcd5
Casey Bodley <cbodley@redhat.com>
2021-10-27 21:02:03 -0400

    update docs
    Signed-off-by: Casey Bodley <cbodley@redhat.com>

--- search/all_5.js
+++ search/all_5.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['field_47',['field',['../classnexus_1_1h3_1_1field.html',1,'nexus::h3']]],
-  ['fields_48',['fields',['../classnexus_1_1h3_1_1fields.html',1,'nexus::h3::fields'],['../classnexus_1_1h3_1_1fields.html#aaae1480e563e304ef35bd2fe2b14bad8',1,'nexus::h3::fields::fields()=default'],['../classnexus_1_1h3_1_1fields.html#a26585bfbf173293cee2a822524732d12',1,'nexus::h3::fields::fields(fields &amp;&amp;o)=default']]],
+  ['fields_48',['fields',['../classnexus_1_1h3_1_1fields.html#aaae1480e563e304ef35bd2fe2b14bad8',1,'nexus::h3::fields::fields()=default'],['../classnexus_1_1h3_1_1fields.html#a26585bfbf173293cee2a822524732d12',1,'nexus::h3::fields::fields(fields &amp;&amp;o)=default'],['../classnexus_1_1h3_1_1fields.html',1,'nexus::h3::fields']]],
   ['find_49',['find',['../classnexus_1_1h3_1_1fields.html#aa68d63d6574e22e3c57f10839540c93b',1,'nexus::h3::fields::find(std::string_view name)'],['../classnexus_1_1h3_1_1fields.html#a92fb0a77d6025cbed989ff9e1d20d9d4',1,'nexus::h3::fields::find(std::string_view name) const']]],
   ['flush_50',['flush',['../classnexus_1_1quic_1_1stream.html#aafe8afbda40ac96653d4da92a5bcc745',1,'nexus::quic::stream::flush(error_code &amp;ec)'],['../classnexus_1_1quic_1_1stream.html#a072931593deae276645944bddc3a5832',1,'nexus::quic::stream::flush()']]],
   ['front_51',['front',['../classnexus_1_1quic_1_1connection__id.html#a9d04400e265195cee6c4c31c328b9de9',1,'nexus::quic::connection_id::front()'],['../classnexus_1_1quic_1_1connection__id.html#a11c26c419f21dc5661ca1d12dc1b47f7',1,'nexus::quic::connection_id::front() const']]]
--- search/functions_c.js
+++ search/functions_c.js
@@ -4,7 +4,7 @@ var searchData=
   ['operator_21_3d_195',['operator!=',['../classnexus_1_1quic_1_1connection__id.html#a69d1f5641b11d830d6440384d91165be',1,'nexus::quic::connection_id']]],
   ['operator_3c_196',['operator&lt;',['../classnexus_1_1quic_1_1connection__id.html#a91e80418f3f2ad4e312b3ece05368260',1,'nexus::quic::connection_id']]],
   ['operator_3c_3d_197',['operator&lt;=',['../classnexus_1_1quic_1_1connection__id.html#abc900d2e5169c325d4769a4daf2f82e7',1,'nexus::quic::connection_id']]],
-  ['operator_3d_198',['operator=',['../classnexus_1_1global_1_1context.html#a734091fa34c4bd6f9ff0eed5e7371430',1,'nexus::global::context::operator=()'],['../classnexus_1_1h3_1_1fields.html#a6a6b8d909044e55b9e9f717cdb7984b0',1,'nexus::h3::fields::operator=()'],['../classnexus_1_1quic_1_1connection__id.html#ade9e5b6aa65ee41b2fe804d5b1783ab2',1,'nexus::quic::connection_id::operator=()']]],
+  ['operator_3d_198',['operator=',['../classnexus_1_1global_1_1context.html#a734091fa34c4bd6f9ff0eed5e7371430',1,'nexus::global::context::operator=()'],['../classnexus_1_1h3_1_1fields.html#a6a6b8d909044e55b9e9f717cdb7984b0',1,'nexus::h3::fields::operator=()'],['../classnexus_1_1quic_1_1connection__id.html#ade9e5b6aa65ee41b2fe804d5b1783ab2',1,'nexus::quic::connection_id::operator=(const connection_id &amp;) noexcept=default']]],
   ['operator_3d_3d_199',['operator==',['../classnexus_1_1quic_1_1connection__id.html#a96dd77ff86200b30fe7a7bc713d03c12',1,'nexus::quic::connection_id']]],
   ['operator_3e_200',['operator&gt;',['../classnexus_1_1quic_1_1connection__id.html#afd7ccfe9becdce707453959384b36d08',1,'nexus::quic::connection_id']]],
   ['operator_3e_3d_201',['operator&gt;=',['../classnexus_1_1quic_1_1connection__id.html#ae5b033269d0e18ee0273c5c9c5080102',1,'nexus::quic::connection_id']]],
